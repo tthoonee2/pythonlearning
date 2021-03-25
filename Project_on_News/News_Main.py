5add2d6b54bb5a2d9f529b4cb78e1b7daef92a47
tthoonee2 <80969262+tthoonee2@users.noreply.github.com>
2021-03-25 01:42:28 +0100

    tropp stonks

--- Project_on_News/News_Main.py
+++ Project_on_News/News_Main.py
@@ -1,11 +1,16 @@
-import requests
+
 import urllib.request
 import re
 
+
 paragraph_vector = []
+#creazione e apertura di un file di testo per semplificare il processo di scrittura delle stringhe dei paragrafi
+
+file = open("extracted_paragraphs.txt", "w")
+file = open("extracted_paragraphs.txt", "a")
 
 #da sistemare a mo di funzione
-link = 'https://www.milanotoday.it/attualita/coronavirus/vaccini-covid-domicilio-medici-famiglia-base.html'
+link = input('inserisci il link:\n')
 link_management = urllib.request.urlopen(link)
 html_page = link_management.read()
 #print(html_page) #restituzione in tipo Byte
@@ -14,17 +19,31 @@ print(html_page)
 
 #prima scrematura - ricavo i paragrafi:
 
-paragraphs_vector = html_page.split('<p>')
-paragraphs_vector = paragraphs_vector.strip()
-print("this is a new string", paragraphs_vector, "\n \n \n")
+regex = r"<p>.+</p>"
+
+test_str = html_page
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+matches = re.finditer(regex, test_str)
+
+for matchNum, match in enumerate(matches, start=1):  #ITS REGEX TIMEEEE!
+    
+    file.write("Match {matchNum} was found at {start}-{end}: {match} \n ".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        file.write("Group {groupNum} found at {start}-{end}: {group} \n".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
+
 
 
 
 #we need:
-#error handling
-#title retrieving
-#html retrieving
-#<p> retrieving 
+#error handling - not done
+#title retrieving - not done
+#html retrieving - done
+#<p> retrieving - done --> written on file, shall i convert this to writing it on a string and that's it? or a json?
+#second part: retrieving VIA
 
 
 
