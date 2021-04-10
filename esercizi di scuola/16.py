6a98a6c22f5059453c562a2b8aaeec875e247c32
tthoonee2 <80969262+tthoonee2@users.noreply.github.com>
2021-04-10 17:36:36 +0200

    Create 16.py

---
+++ esercizi di scuola/16.py
@@ -0,0 +1,16 @@
+def name_output():
+    nm = input('insert the first name: ')
+    ln = input('insert yhe last name: ')
+    print('nice to meet you',nm,ln)
+name_output()
+
+
+
+name = input('insert your name: ')
+lastname = input('insert your last name: ')
+
+#phrase = 'Nice to meet you ' + name + ' ' + lastname
+#print(phrase)
+
+print('NIce to meet you', name, lastname)
+
